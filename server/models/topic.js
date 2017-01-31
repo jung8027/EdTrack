8dcaee2c68ee1343cbb282aa77e536c364eab4c2
electrone901 <Carbajalluis@rocketmail.com>
2017-01-31 17:31:32 -0500

    closes #10

---
+++ server/models/topic.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = (sequelize, DataTypes)=> {
+	const Topic = sequelize.define("Topic", {
+		name: DataTypes.STRING
+	}, {
+		classMethods: {
+	      associate: function(models) {
+	        // Topic.hasMany(models.Student);
+	        // Topic.hasMany(models.Mentor);
+	      }
+	    }
+	}
+	);
+
+	return Topic;
+};
+
