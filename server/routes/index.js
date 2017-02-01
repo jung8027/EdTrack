067c58a5c488aa262c5edec9312f107d1a6782c7
iliass <iliass24@gmail.com>
2017-02-01 17:51:06 -0500

    close #24

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router  = express.Router();
 
-router.use("/topic", require('./TopicRouter.js'));
-router.use("/mentor", require('./MentorRouter.js'));
-router.use("/grade", require('./GradeRouter.js'));
+// router.use("/topic", require('./TopicRouter.js'));
+// router.use("/mentor", require('./MentorRouter.js'));
+// router.use("/grade", require('./GradeRouter.js'));
 router.use("/student", require('./StudentRouter.js'));
 
 module.exports = router;
