a1492b48b67238720cd691bdc36515505a6297c3
electrone901 <Carbajalluis@rocketmail.com>
2017-02-03 21:26:55 -0500

    close#43

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -4,6 +4,7 @@ import App from '../features/app/App';
 import Student from '../features/student/Student';
 import Mentor from '../features/mentor/Mentor';
 import Grade from '../features/grade/Grade';
+import TopicForm from '../features/topic/TopicForm';
 
 
 export default (
@@ -11,5 +12,7 @@ export default (
 		<IndexRoute component={Student}/>
 		<Route path='/mentor' component={Mentor}/>
 		<Route path='/grade' component={Grade}/>
+		<Route path='/grade' component={Grade}/>
+		<Route path='/topic' component={TopicForm}/>
 	</Route>
 );
