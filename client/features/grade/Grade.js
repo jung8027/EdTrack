614ac7fd175edc9119adce95bf9bfdede039e66e
QuanAVuong <quan.a.vuong@gmail.com>
2017-02-07 20:27:15 -0500

    udpate grade view

--- client/features/grade/Grade.js
+++ client/features/grade/Grade.js
@@ -3,22 +3,17 @@ import React, {PropTypes} from 'react';
 
 const Grade = (props)=>{
 		// props has no 'grade' key, props.grade = undefined
-		console.log('grade component props',props);
+		console.log('grade component props', props);
 		return (
 			<div>
 				<h1>Grade Info</h1>
-				{!props.grade.data ?
+				{!props.grade ?
 					<p>Loading...</p> :
-					<ul>
-						<ul>Grades:
-							{props.grade.data.grade.map((grade,indx)=>
-								(<li key={indx}>
-									{grade.type}:
-									{grade.grade}
-
-								</li>))}
-						</ul>
-					</ul>
+					<div>
+						<h3>Hey Student {props.grade.StudentId}, </h3>
+						<h4> You got a <strong>{props.grade.grade}</strong> on your <stong>{props.grade.type}</stong>
+						</h4>
+					</div>
 				}
 			</div>
 		);
