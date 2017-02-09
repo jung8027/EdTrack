55efe13df1b4cab9d6dad80a6647dca9240f54db
electrone901 <Carbajalluis@rocketmail.com>
2017-02-08 20:22:11 -0500

    close#79

---
+++ server/seeders/ClassSeed.js
@@ -0,0 +1,10 @@
+const models = require('../models');
+
+models.Class.sync({force: true})
+.then(() => models.Class.bulkCreate([
+	{name: 'Full Stack', startDate: '01-01-2017', endDate:'11-01-2017'},
+	{name: 'Android',    startDate: '01-01-2017', endDate:'11-01-2017'},
+	{name: 'iOS',        startDate: '01-01-2017', endDate:'11-01-2017'},
+]))
+// if everything is correct, you should only have three classes in your database
+.catch((err) => console.log(err));
