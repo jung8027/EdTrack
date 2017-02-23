b0533bfee09b34c5abd9e6a632bb89f3a9a3082f
Jung Chen <jungwchen@gmail.com>
2017-02-23 18:39:03 -0500

    added more quizes

--- server/seeders/studentSeed.js
+++ server/seeders/studentSeed.js
@@ -3,20 +3,67 @@ const models = require('../models');
 const StudentSeedFunc = () => {
 	//add the following students to the database:
 	models.Student.sync({force: true})
-		.then(() => {
-			models.Student.bulkCreate([
-				{name: 'Iliass', email: 'Iliass@email.com', address: '11 main st'},
-				{name: 'Quan', email: 'Quan@email.com', address: '33 main st'},
-				{name: 'Jung', email: 'Jung@email.com', address: '44 main st'},
-				{name: 'Luis', email: 'Luis@email.com', address: '12 main st'},
-				{name: 'John', email: 'John@email.com', address: '13 main st'},
-				{name: 'Joe', email: 'Joe@email.com', address: '14 main st'},
-				{name: 'Jack', email: 'Jack@email.com', address: '15 main st'},
-				{name: 'Kelly', email: 'Kelly@email.com', address: '16 main st'},
-				{name: 'Tracy', email: 'Tracy@email.com', address: '17 main st'},
-				{name: 'Jill', email: 'Jill@email.com', address: '18 main st'}
-			]);
-		});
+		.then((data) =>
+			models.Student.create({
+				name: 'Iliass', email: 'Iliass@email.com', address: '11 main st'
+			}))
+		.then((student) =>
+			student.addTopics([8,9,10]))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Quan', email: 'Quan@email.com', address: '33 main st'
+			}))
+		.then((student) =>
+			student.addTopics([2,4,10]))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Jung', email: 'Jung@email.com', address: '44 main st'
+			}))
+		.then((student) =>
+			student.addTopics([2,3,4]))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Luis', email: 'Luis@email.com', address: '12 main st'
+			}))
+		.then((student) =>
+			student.addTopics([2,6,9]))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'John', email: 'John@email.com', address: '13 main st'
+			}))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Joe', email: 'Joe@email.com', address: '14 main st'
+			}))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Jack', email: 'Jack@email.com', address: '15 main st'
+			}))
+		.then((student) =>
+			student.addTopics([2,6,9]))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Kelly', email: 'Kelly@email.com', address: '16 main st'
+			}))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Tracy', email: 'Tracy@email.com', address: '17 main st'
+			}))
+
+		.then((data) =>
+			models.Student.create({
+				name: 'Jill', email: 'Jill@email.com', address: '18 main st'
+			}))
+		.then((student) =>
+			student.addTopics([3,6,9]));
 };
 StudentSeedFunc();
 module.exports = StudentSeedFunc;
